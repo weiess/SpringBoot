e4fbb9e3010fcb2355468bd57b3897985b9c7e9e
yang <18915924001@163.com>
2019-08-31 21:51:14 +0800

    first commit

---
+++ springboot-demoweb/src/main/java/com/yzl/SpringbootDemowebApplication.java
@@ -0,0 +1,14 @@
+package com.yzl;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemowebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemowebApplication.class, args);
+    }
+
+}
---
+++ springboot-demoweb/src/test/java/com/yzl/serviceImpl/BlogServiceImplTest.java
@@ -0,0 +1,7 @@
+package com.yzl.serviceImpl;
+
+import static org.junit.Assert.*;
+
+public class BlogServiceImplTest {
+
+}
